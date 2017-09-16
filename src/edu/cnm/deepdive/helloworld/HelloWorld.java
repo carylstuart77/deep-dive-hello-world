521fdae686a01ddaeb068dd848b66dd9d7b19b20
caryl <caryl@192.168.0.172>
2017-09-16 17:46:48 -0600

    First project implemented "Hello World" PreWork program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * JAS Java Hello World project.
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to standard output.
+   * @param args  Command line ignored
+   */
+  public static void main(String[] args) {
+    // "Hello World!"
+    System.out.println("Hello World!");
+  }
+
+}
